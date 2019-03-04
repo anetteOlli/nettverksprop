900951e7a3a6e553102d09ce2b67499fd9e196c6
elisamop <elisamop@stud.ntnu.no>
2019-03-04 14:02:41 +0100

    nå er vi egentlig ferdig trenger bare fikse litt på de tomme listepunktene?

--- Oving3/websocket.go
+++ Oving3/websocket.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -30,9 +31,30 @@ func main() {
 }
 
 func handleClient(conn net.Conn){
-	var body string = "<!DOCTYPE html><HTML><body><H1> Hilsen. Du har koblet deg opp til min enkle web-tjener </h1>Header fra klient er:</body></HTML>\r\n\r\n"
-	var header string = "HTTP/1.0 200 OK\r\nContent-Type: text/html \r\nContent-length: " + strconv.Itoa(len(body)) + "\r\n\r\n"
-	fmt.Print(header, body)
+	var bodyStart  = "<!DOCTYPE html><HTML><body><H1> Hilsen. Du har koblet deg opp til min enkle web-tjener </h1>Header fra klient er: <ul>"
+	var bodyEnd  = "</ul></body></HTML>\r\n\r\n"
+
+
+	var buf [512]byte
+	fmt.Println(buf)
+	conn.Read(buf[0:])
+	fmt.Println(buf)
+	var bufRead = string(buf[0:])
+	fmt.Println(bufRead)
+
+	temp := strings.Split(bufRead,"\n")
+	var bodyMid= ""
+	for _, element := range temp {
+		var el = strings.TrimSpace(element)
+		if len(el) !=0 && el != "  " {
+			bodyMid += "<li>" + el + "</li>"
+		}
+	}
+
+	var body = bodyStart + bodyMid + bodyEnd
+
+	var header  = "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\ncharset=UTF-8\r\nContent-length: " + strconv.Itoa(len(body)) + "\r\n\r\n"
+	fmt.Println(header, body)
 	var total = header + body
 
 	conn.Write([]byte(total)) // don't care about return value
