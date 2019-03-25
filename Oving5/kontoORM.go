815acca131b6f1029fb0aadfa11c2812563c4e6a
anettosi <sukkerbombe@hotmail.com>
2019-03-25 17:33:08 +0100

    endret main slik at metoder er mer tilgjengelig

--- Oving5/kontoORM.go
+++ Oving5/kontoORM.go
@@ -26,33 +26,90 @@ type konto struct {
 
 
 func main(){
-	mysqlAdress := "anettosi:LN8iIcr6@tcp(mysql.stud.iie.ntnu.no:3306)/anettosi?charset=utf8&parseTime=True&loc=Local"
-	db, err :=gorm.Open("mysql", mysqlAdress)
-	defer db.Close() //lukker db for oss når vi er ferdig med den for oss, trenger ikke å ha db.close() på slutten av funksjonen som vi må i java
-	if err != nil{
-		fmt.Print("Connection failed to open   ", err)
+	var rikeKontoer []konto
+	db, err := OpprettForbindelse()
+	if err !=nil {
 		return
 	}
-	fmt.Print("connection established")
+	defer db.Close()
 
 	//db.Debug().DropTableIfExists() vs db.DropTableIfExists() --> debug() skriver ut sql spørringa
-	db.Debug().DropTableIfExists(&konto{}) //drop tables if they exists
-	db.Debug().AutoMigrate(&konto{}) //auto creates tables based on struct konto
+	//db.Debug().DropTableIfExists(&konto{}) //drop tables if they exists
+	//db.Debug().AutoMigrate(&konto{}) //auto creates tables based on struct konto
 
 	fortsette := true
 	for fortsette{
 		reader := bufio.NewReader(os.Stdin) //tilsvarer java scanner
-		fmt.Print("for å legge inn person i db skriv ADD, \n for å slette skriv DELETE, \n for å overføre penger skriv TRANSFER, \n for å endre navn CHANGE,\nfor å avslutte skriv STOP")
+		fmt.Print("for å legge inn person i db skriv ADD, \n for å slette skriv DELETE, \n for å overføre penger skriv TRANSFER,\n for å hente ut en bruker, skriv GETONE,\n for å endre navn CHANGE,\nfor å avslutte skriv STOP")
 		valg,_:=reader.ReadString('\n')
 		switch valg {
 		case "ADD\n":
-			lagBruker(db)
+			fmt.Print("skriv inn navnet på eier av konto")
+			navnRead, _ := reader.ReadString('\n')
+			navn := strings.Trim(navnRead, "\n")
+			fmt.Print("skriv inn beløp på kontoen:")
+			moneyRead, _ := reader.ReadString('\n')
+			moneyReadTrim :=strings.Trim(moneyRead, "\n")
+			money, err := strconv.Atoi(moneyReadTrim) //konverterer string til int
+			for err!=nil{
+				fmt.Print("skriv inn beløp på kontoen, beløpet må være et heltall")
+				moneyRead, _ = reader.ReadString('\n')
+				moneyReadTrim = strings.Trim(moneyRead, "\n")
+				money, err = strconv.Atoi(moneyReadTrim)
+			}
+			LagBruker(db, navn, money)
 		case "DELETE\n":
-			slettBruker(db)
+			fmt.Print("skriv navnet på brukeren:")
+			navnRead,_ := reader.ReadString('\n')
+			navn :=strings.Trim(navnRead, "\n")
+			SlettBruker(db, navn)
 		case "CHANGE\n":
-			oppdaterNavn(db)
+			fmt.Print("skriv inn det gamle brukernavnet")
+			oldNavnRead, _ := reader.ReadString('\n')
+			oldNavn := strings.Trim(oldNavnRead, "\n")
+			fmt.Print("skriv inn det nye navnet")
+			newNameRead,_:=reader.ReadString('\n')
+			newNavn := strings.Trim(newNameRead, "\n")
+			OppdaterNavn(db, oldNavn, newNavn)
 		case "TRANSFER\n":
-			transferMOney(db)
+			fmt.Print("hvem skal overføre penger?")
+			fmt.Print("hvem skal overføre penger?")
+			donorRead,_:=reader.ReadString('\n')
+			donor :=strings.Trim(donorRead,"\n")
+			fmt.Print("hvem skal få penger?")
+			motakerRead,_:=reader.ReadString('\n')
+			motaker := strings.Trim(motakerRead,"\n")
+			fmt.Print("hvor mye penger skal overføres?")
+			pengerRead,_:=reader.ReadString('\n')
+			pengerRead = strings.Trim(pengerRead, "\n")
+			penger, err := strconv.Atoi(pengerRead)
+			for err!=nil{
+				fmt.Print("du må skrive inn et heltall")
+				pengerRead,_ = reader.ReadString('\n')
+				pengerRead = strings.Trim(pengerRead, "\n")
+				penger, err = strconv.Atoi(pengerRead)
+			}
+			TransferMOney(db, donor, motaker, penger)
+		case "GETONE\n":
+			fmt.Print("skriv inn navn")
+			navnRead,_ := reader.ReadString('\n')
+			navn := strings.Trim(navnRead, "\n")
+			bruker := GetKonto(db, navn)
+			fmt.Print(bruker.Kontonummer, bruker.Kunde, strconv.Itoa(bruker.Penger) + "\n")
+		case "RIKE\n":
+			fmt.Print("hvor rik?, skriv inn minste beløp")
+			pengerRead, _ := reader.ReadString('\n')
+			pengerRead = strings.Trim(pengerRead, "\n")
+			penger, err := strconv.Atoi(pengerRead)
+			for err!=nil{
+				fmt.Print("du må skrive inn et heltall")
+				pengerRead, _ = reader.ReadString('\n')
+				pengerRead = strings.Trim(pengerRead, "\n")
+				penger, err = strconv.Atoi(pengerRead)
+			}
+			rikeKontoer = GetRikeKontoer(db, penger)
+			fmt.Print(rikeKontoer)
+
 		default:
 			fmt.Print("du valgte å avslutte ", valg)
 			fortsette = false
@@ -63,32 +120,15 @@ func main(){
 
 }
 
-func lagBruker(db *gorm.DB){
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("skriv inn navnet på eier av konto")
-	navnRead, _ := reader.ReadString('\n')
-	navn := strings.Trim(navnRead, "\n")
-	fmt.Print("skriv inn beløp på kontoen:")
-	moneyRead, _ := reader.ReadString('\n')
-	moneyReadTrim :=strings.Trim(moneyRead, "\n")
-	money, err := strconv.Atoi(moneyReadTrim) //konverterer string til int
-	for err!=nil{
-		fmt.Print("skriv inn beløp på kontoen, beløpet må være et heltall")
-		moneyRead, _ = reader.ReadString('\n')
-		moneyReadTrim = strings.Trim(moneyRead, "\n")
-		money, err = strconv.Atoi(moneyReadTrim)
-	}
-	fmt.Print("kommet så langt, lest verdiene: " + strconv.Itoa(money) + "  " + navn)
-	person :=&konto{Kunde:navn, Penger:money} //lager et person objekt av konto type
+func LagBruker(db *gorm.DB, navn string, penger int){
+
+	fmt.Print("kommet så langt, lest verdiene: " + strconv.Itoa(penger) + "  " + navn)
+	person :=&konto{Kunde:navn, Penger:penger} //lager et person objekt av konto type
 	db.Debug().Create(person) //opretter person i databasen
 	return
-
 }
-func slettBruker(db *gorm.DB){
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("skriv navnet på brukeren:")
-	navnRead,_ := reader.ReadString('\n')
-	navn :=strings.Trim(navnRead, "\n")
+
+func SlettBruker(db *gorm.DB, navn string){
 
 	person :=&konto{}
 	//First() vs Find(): First() finner kjører med LIMIT 1
@@ -101,61 +141,57 @@ func slettBruker(db *gorm.DB){
 	}else{
 		fmt.Print("personen finnes ikke")
 	}
-
-
 }
-func oppdaterNavn(db *gorm.DB){
-	reader :=bufio.NewReader(os.Stdin)
-	fmt.Print("skriv inn det gamle brukernavnet")
-	navnRead, _ := reader.ReadString('\n')
-	navn := strings.Trim(navnRead, "\n") //hive bort linjeskiftet fra navnet, og ja; dette er ugly
-
+func OppdaterNavn(db *gorm.DB, oldName string, newName string){
 	 var person konto
-	db.Debug().First(&person, "Kunde=?", navn)
+	db.Debug().First(&person, "Kunde=?", oldName)
 	fmt.Print(person.Kunde, person.Kontonummer, person.Penger)
-	fmt.Print("skriv inn nytt navn:")
-	navnRead, _ = reader.ReadString('\n')
-	navn = strings.Trim(navnRead, "\n")
-	person.Kunde = navn
+	person.Kunde = newName
 	db.Debug().Save(&person)
-
 }
-func transferMOney(db *gorm.DB){
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("hvem skal overføre penger?")
-	donorRead,_:=reader.ReadString('\n')
-	donor :=strings.Trim(donorRead,"\n")
+
+func TransferMOney(db *gorm.DB, donor string, mottaker string, penger int){
 
 	donorPerson :=&konto{}
 	db.Debug().First(&donorPerson, "Kunde=?", donor)
 	//her burde man sikkert ha hatt en sjekk på at kunden faktisk finnes, MEEEEN det driter jeg i
 
-	fmt.Print("hvem skal få penger?")
-	motakerRead,_:=reader.ReadString('\n')
-	motaker := strings.Trim(motakerRead,"\n")
-	motakerPerson :=&konto{}
-	db.Debug().First(&motakerPerson, "Kunde=?", motaker)
+	mottakerPerson :=&konto{}
+	db.Debug().First(&mottakerPerson, "Kunde=?", mottaker)
 	//igjen her mangler det en del if-sjekker, men de bryr vi oss ikke om
-
-	fmt.Print("hvor mye penger skal overføres?")
-	pengerRead,_:=reader.ReadString('\n')
-	pengerRead = strings.Trim(pengerRead, "\n")
-	penger, err := strconv.Atoi(pengerRead)
-	for err!=nil{
-		fmt.Print("du må skrive inn et heltall")
-		pengerRead,_ = reader.ReadString('\n')
-		pengerRead = strings.Trim(pengerRead, "\n")
-		penger, err = strconv.Atoi(pengerRead)
-	}
 
 	//her får man lov til å overføre mer penger enn man har, null stress med minus på konto
 	donorPerson.Penger = donorPerson.Penger - penger
-	motakerPerson.Penger = motakerPerson.Penger + penger
+	mottakerPerson.Penger = mottakerPerson.Penger + penger
 
-	
 	//så bare lagre dette i databasen og så er vi good
 	db.Debug().Save(&donorPerson)
-	db.Debug().Save(&motakerPerson)
+	db.Debug().Save(&mottakerPerson)
 
 }
 
+func OpprettForbindelse() (*gorm.DB, error){
+	mysqlAdress := "anettosi:LN8iIcr6@tcp(mysql.stud.iie.ntnu.no:3306)/anettosi?charset=utf8&parseTime=True&loc=Local"
+	db, err :=gorm.Open("mysql", mysqlAdress)
+	//defer db.Close() //lukker db for oss når vi er ferdig med den for oss, trenger ikke å ha db.close() på slutten av funksjonen som vi må i java
+	if err != nil{
+		fmt.Print("Connection failed to open   ", err)
+		return db, err
+	}
+	fmt.Print("connection established")
+	return db, nil
+}
+
+func GetKonto(db *gorm.DB, navn string)konto{
+	var konto konto
+	 db.Debug().First(&konto, "Kunde=?", navn)
+	return konto
+}
+func GetRikeKontoer(db *gorm.DB,  penger int)[]konto{
+	var kontoer []konto
+	db.Debug().Find(&kontoer, "Penger>=?", penger)
+	return kontoer
+}
+func WipeDatabase(db *gorm.DB){
+	db.Debug().DropTableIfExists(&konto{})
+}
