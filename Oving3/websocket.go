3b03a0b81d1d0d7d0d1c3c5e810ca8523bc366c7
Anette Olli Siiri <anettosi@stud.ntnu.no>
2019-04-05 15:27:16 +0200

    startet Oving7

--- Oving3/websocket.go
+++ Oving3/websocket.go
@@ -12,13 +12,14 @@ import (
 
 func main() {
 
+	
 	service := ":1200"
+
 	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
 	checkError(err)
 
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	checkError(err)
-
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
